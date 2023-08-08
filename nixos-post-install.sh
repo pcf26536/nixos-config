892476bdf37f4252f9a8068201f51f775be9b03d
pcf26536 <muffwaindan@gmail.com>
2023-08-08 04:34:10 +0300

    mcfly init

--- nixos-post-install.sh
+++ nixos-post-install.sh
@@ -1,2 +1,5 @@
 sudo nix-channel --add https://github.com/nix-community/home-manager/archive/release-23.05.tar.gz home-manager
 sudo nix-channel --update
+# mcfly: configuration.nix does not work as of 23.05
+eval "$(mcfly init zsh)"
+eval "$(mcfly init bash)"
