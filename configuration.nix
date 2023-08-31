9bcf84593b1456b14080daac4ba5777d60d6da40
pcf26536 <muffwaindan@gmail.com>
2023-09-01 02:24:50 +0300

    remove unused configs

--- configuration.nix
+++ configuration.nix
@@ -237,12 +237,6 @@
       alsa.enable = true;
       alsa.support32Bit = true;
       pulse.enable = true;
-      # If you want to use JACK applications, uncomment this
-      #jack.enable = true;
-
-      # use the example session manager (no others are packaged yet so this is enabled by default,
-      # no need to redefine it in your config for now)
-      #media-session.enable = true;
     };
   };
 
@@ -450,14 +444,6 @@
         "nvidia-persistenced" # the daemon prevents the driver from releasing device state when the device is not in use
       ];
   };
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
   # Virtualisation
   virtualisation = {
